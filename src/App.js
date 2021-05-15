25c339e48545be19deeb9e4365e8a2c76d3541a6
ohadstoller <ohadstoller@gmail.com>
2021-05-15 19:24:12 +0300

    adding useEffects and axiosget method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import Note from "./components/Note";
+import axios from "axios";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('a new note')
-  const [showAll, setShowAll] = useState(true)
+  const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes: ', notes)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -20,7 +32,6 @@ const App = (props) => {
 
   const isImportant = note => note.important;
   const notesToShow = showAll ? notes : notes.filter(isImportant)
-
 
 
   const handleNoteChange = (event) => {
